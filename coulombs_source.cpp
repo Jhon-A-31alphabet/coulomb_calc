710ca4eca37f7561379cd334650a5c5776d3fb0a
Jhon Marcos <kenzhait@gmail.com>
2023-01-04 10:17:09 -0800

    menu creado

---
+++ coulombs_source.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+#include<cmath>
+using namespace std;
+
+
+void menu();
+int main(){
+    menu();
+}
+
+void menu(){
+    cout<<"Manejo del calculo de la fuerza de atraccion"<<endl<<"de particulas subatomicas"<<endl;
+    cout<<"utilizando la ley de coulomb. (vamos a medir culombios)"<<endl;
+    cout<<"La formula de la ley de coulomb es:      F = K* (Q1*Q2) / R**2"<<endl<<endl;
+}
